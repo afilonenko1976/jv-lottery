b881b05caf8759e01e6d0c4051fa4b6bbd80ec6e
Andrei Filonenko <a1.sankai@gmail.com>
2025-06-29 09:08:31 +0300

    Fix task

--- src/main/java/core/basesyntax/ColorSupplier.java
+++ src/main/java/core/basesyntax/ColorSupplier.java
@@ -4,13 +4,11 @@ import java.util.Random;
 
 public class ColorSupplier {
 
-    public static String getRandomColor() {
+    public Color getRandomColor() {
 
         int index = new Random().nextInt(Color.values().length);
         Color color = Color.values()[index];
-        System.out.println(color);
 
-        return String.valueOf(color);
-
+        return color;
     }
 }
--- src/main/java/core/basesyntax/Lottery.java
+++ src/main/java/core/basesyntax/Lottery.java
@@ -6,7 +6,9 @@ public class Lottery {
 
     public static Ball getRandomBall() {
 
-        String color = ColorSupplier.getRandomColor();
+        ColorSupplier colorSupplier = new ColorSupplier();
+
+        Color color = colorSupplier.getRandomColor();
         int randomNumber = new Random().nextInt(100);
 
         Ball ball = new Ball(color,randomNumber);
